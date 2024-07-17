b7b3146ef96d8c1d1cb2400f78349fa6524a25ee
Rubin  Ramallari <161735134+rubinramallari9@users.noreply.github.com>
2024-07-17 10:17:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import requests
+import serial
+import time
 
 
+arduino_port = 'COM3'
+baud_rate = 9600
+ser = serial.Serial(arduino_port, baud_rate)
+try:
+    while True:
+        response = requests.get("https://api.coincap.io/v2/assets/bitcoin").json()
+        price = response["data"]["priceUsd"].split(".")[0]
+        print(price)
+        time.sleep(2)
 
-response = requests.get("https://api.coincap.io/v2/assets/bitcoin").json()
-price = response["data"]["priceUsd"].split(".")[0]
-print(price)
+        def send_message_to_lcd(message):
+            ser.write(message.encode() + b'\n')
+
+        send_message_to_lcd("BTC: "+price+"$")
+        time.sleep(5)
+
+except serial.SerialException as e:
+    print(f"Error opening serial port: {e}")
+
+finally:
+    ser.close()
